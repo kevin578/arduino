ba0455fb1262acc4850dae6863b1528153adcbaf
kevin578 <kbriggs@aapl000894.local>
2018-01-28 19:41:06 -0500

    added todos

--- tori_sophia/tori_sophia.ino
+++ tori_sophia/tori_sophia.ino
@@ -3,6 +3,7 @@ const int rs = 8, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 
+// TODO #1 create a variable called moneyNeeded and set it to 50
  
 void setup() {
   // put your setup code here, to run once:
@@ -10,34 +11,35 @@ void setup() {
  pinMode(10, OUTPUT);
  pinMode(13, OUTPUT);
  Serial.begin(9600);
- //pinMode(2, INPUT_PULLUP);
- attachInterrupt(digitalPinToInterrupt(2), coinCollect, FALLING);
+ pinMode(2, INPUT_PULLUP);
+ // create an attactInterupt.
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-   lcd.setCursor(1, 0);
-   lcd.print("Please insert ");
-   lcd.setCursor(4,1);
-   lcd.print("50 cents");
-   analogWrite(10,50);
-  moveMotor();
-  delay(5000);
+// TODO #2 - uncomment and the money needed should be fifty 
+//   if (moneyNeeded == 50) {
+//    lcd.setCursor(1, 0);
+//    lcd.print("Please insert ");
+//    lcd.setCursor(4,1);
+//    lcd.print("50 cents");
+//   }
+    //add other else if statements
+
+// TODO #3 - add the else if statements
+   
 }
 
-void countChange(){
-Serial.println("Coin");
-}
 
 void moveMotor(){
+  analogWrite(10,50);
   digitalWrite(13,HIGH);
   delay(2000);
   digitalWrite(13,LOW);
 }
 
-void coinCollect() {
-  Serial.println("coin accepted");
-}
+//call function
+
 
 
 
