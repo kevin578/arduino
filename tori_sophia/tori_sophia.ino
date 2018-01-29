eab3d9e5145563d65aff5fb6f4448fdb621fd2d1
kevin578 <kbriggs@aapl000894.local>
2018-01-29 17:42:30 -0500

    added button

--- tori_sophia/tori_sophia.ino
+++ tori_sophia/tori_sophia.ino
@@ -3,42 +3,62 @@ const int rs = 8, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 
-// TODO #1 create a variable called moneyNeeded and set it to 50
+int moneyNeeded= 50;
  
 void setup() {
   // put your setup code here, to run once:
  lcd.begin(16, 2);
  pinMode(10, OUTPUT);
+ pinMode(12, OUTPUT);
  pinMode(13, OUTPUT);
  Serial.begin(9600);
  pinMode(2, INPUT_PULLUP);
+ pinMode(A0, INPUT_PULLUP);
  // create an attactInterupt.
+  attachInterrupt(digitalPinToInterrupt(2), coinCollect, FALLING);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-// TODO #2 - uncomment and the money needed should be fifty 
-//   if (moneyNeeded == 50) {
-//    lcd.setCursor(1, 0);
-//    lcd.print("Please insert ");
-//    lcd.setCursor(4,1);
-//    lcd.print("50 cents");
-//   }
-    //add other else if statements
+   if (moneyNeeded == 50) {
+    lcd.setCursor(1, 0);
+    lcd.print("Please insert ");
+    lcd.setCursor(4,1);
+    lcd.print("50 cents");
+   }
+ else if( moneyNeeded == 25) {
+    lcd.setCursor(1, 0);
+    lcd.print("Please insert ");
+    lcd.setCursor(4,1);
+    lcd.print("25 cents");
+ }
+ else if(moneyNeeded == 0) {
+    lcd.setCursor(1, 0);
+    lcd.print("");
+    lcd.setCursor(4,1);
+    lcd.print("");
+  if(digitalRead(A0) == LOW) {
+     moveMotor(12);
+     moneyNeeded = 50;
+  }
 
+ }
 // TODO #3 - add the else if statements
-   
 }
 
 
-void moveMotor(){
+void moveMotor(int pinNumber){
   analogWrite(10,50);
-  digitalWrite(13,HIGH);
+  digitalWrite(pinNumber,HIGH);
   delay(2000);
-  digitalWrite(13,LOW);
+  digitalWrite(pinNumber,LOW);
 }
 
-//call function
+void coinCollect(){
+  moneyNeeded = moneyNeeded - 5; 
+}
+
+
 
 
 
